c896b2ad1c3907c05b9c47699a27c41a69c1db77
mmorfey <mmorfey@gmail.com>
2021-10-21 16:51:25 +0300

    add hw3 Array & Bingo

--- src/com/pb/taran/hw3/Array.java
+++ src/com/pb/taran/hw3/Array.java
@@ -21,37 +21,52 @@ public class Array {
         //Программа должна позволить пользователю ввести одномерный массив целых чисел размерностью 10 элементов.
         int[] arrayList = new int[10];
 
-        for (int i = 0; i < arrayList.length; i++)
-        {
-            System.out.print("Please enter number " + (i+1) + ": ");
+        for (int i = 0; i < arrayList.length; i++) {
+            System.out.print("Please enter number " + (i + 1) + ": ");
             arrayList[i] = scan.nextInt();
         }
-        System.out.println("Array list: " + Arrays.toString(arrayList));
+        System.out.println("Array list (toString): " + Arrays.toString(arrayList));
+
+        System.out.print("Array list (for): ");
+        for (int i = 0; i < arrayList.length; i++) {
+            if (i < arrayList.length - 1) {
+                System.out.print(arrayList[i] + ", ");
+            } else {
+                System.out.print(arrayList[i]);
+            }
+        }
+        System.out.println();
+        //Подсчитать сумму всех элементов массива и вывести ее на экран.
+        //Подсчитать и вывести на экран количество положительных элементов.
         System.out.println("Array sum (stream): " + Arrays.stream(arrayList).sum());
 
+        int counter = 0;
         int sum = 0;
-        for (int i = 0; i < arrayList.length; i++) {
-            sum = sum + arrayList[i];
+//        for (int i = 0; i < arrayList.length; i++) { //for
+//            if (arrayList[i] > 0) {
+//                counter += 1;
+//            }
+//            sum += arrayList[i];
+//        }
+        for (int num : arrayList) { //foreach
+            if (num > 0) {
+                counter += 1;
+            }
+            sum += num;
         }
         System.out.println("Array sum (for): " + sum);
-
-        //Подсчитать и вывести на экран количество положительных элементов.
-        int counter = 0;
-        for(int i = 0; i<arrayList.length; i++)
-        {
-            if(arrayList[i] > 0)
-                counter += 1;
-        }
         System.out.println("Positive array numbers: " + counter);
 
         //Произвести сортировку этого массива от меньшего к большему по алгоритму сортировки пузырьком. (https://en.wikipedia.org/wiki/Bubble_sort)
-        for (int i = 0; i < arrayList.length-1; i++)
-            for (int j = 0; j < arrayList.length-i-1; j++)
-                if (arrayList[j] > arrayList[j+1]) {
+        for (int i = 0; i < arrayList.length - 1; i++) {
+            for (int j = 0; j < arrayList.length - i - 1; j++) {
+                if (arrayList[j] > arrayList[j + 1]) {
                     int temp = arrayList[j];
-                    arrayList[j] = arrayList[j+1];
-                    arrayList[j+1] = temp;
+                    arrayList[j] = arrayList[j + 1];
+                    arrayList[j + 1] = temp;
                 }
+            }
+        }
         //Вывести на экран отсортированный массив.
         System.out.println("Array sort: " + Arrays.toString(arrayList));
     }
