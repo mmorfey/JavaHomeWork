5f626c52ca1d07ec1e217d56317916634faf8e5e
mmorfey <mmorfey@gmail.com>
2022-01-16 01:27:56 +0200

    add hw15 v.2 ChatServer

---
+++ src/com/pb/taran/hw15/RunClient.java
@@ -0,0 +1,8 @@
+package com.pb.taran.hw15;
+
+public class RunClient {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+        ClientWindow clientWindow2 = new ClientWindow();
+    }
+}
---
+++ src/com/pb/taran/hw15/RunServer.java
@@ -0,0 +1,7 @@
+package com.pb.taran.hw15;
+
+public class RunServer {
+    public static void main(String[] args) {
+        ChatServer server = new ChatServer();
+    }
+}
